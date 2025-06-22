9ff6174877316c6a60163b75bc6880c30b01f406
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-06-22 15:29:52 +0100

    Create products.ts

---
+++ data/products.ts
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: "1",
+    name: "Smartwatch X10",
+    price: "$149",
+    image: "/images/watch.jpg",
+  },
+  {
+    id: "2",
+    name: "Noise Cancelling Headphones",
+    price: "$199",
+    image: "/images/headphones.jpg",
+  },
+  {
+    id: "3",
+    name: "NextGen Smartphone",
+    price: "$899",
+    image: "/images/smartphone.jpg",
+  },
+  {
+    id: "4",
+    name: "Portable Speaker Pro",
+    price: "$89",
+    image: "/images/speaker.jpg",
+  },
+  {
+    id: "5",
+    name: "Gadget Backpack",
+    price: "$59",
+    image: "/images/backpack.jpg",
+  },
+  {
+    id: "6",
+    name: "Wireless Charger Pad",
+    price: "$39",
+    image: "/images/charger.jpg",
+  },
+];
+
+export default products;
