c9466484be7a02376eade076f3186e30eec1e43b
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-06-22 15:21:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white text-black">
       <Hero />
-      {/* Add FeaturedProducts and more later */}
+      
       <Footer />
     </main>
   );
