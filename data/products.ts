96ada93c1702304a8cfd5379fda4a24ecbd4af76
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-05 18:10:18 +0100

    Update products.ts

--- data/products.ts
+++ data/products.ts
@@ -4,36 +4,42 @@ const products = [
     name: "Smartwatch X10",
     price: "$149",
     image: "/images/smartwatch.jpg",
+    category: "Wearables",
   },
   {
     id: "2",
     name: "Noise Cancelling Headphones",
     price: "$199",
     image: "/images/NoiseCancelHeadphone.jpg",
+    category: "Audio",
   },
   {
     id: "3",
-    name: "iPhone 16 pro max",
+    name: "iPhone 16 Pro Max",
     price: "$899",
     image: "/images/i16.jpg",
+    category: "Phones",
   },
   {
     id: "4",
     name: "Portable Speaker Pro",
     price: "$89",
     image: "/images/speaker.jpg",
+    category: "Audio",
   },
   {
     id: "5",
     name: "Gadget Backpack",
     price: "$59",
     image: "/images/backpack.jpg",
+    category: "Accessories",
   },
   {
     id: "6",
     name: "Wireless Charger Pad",
     price: "$39",
     image: "/images/wirelessCharger.jpg",
+    category: "Accessories",
   },
 ];
 
