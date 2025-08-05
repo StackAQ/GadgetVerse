321fe296b9672a4098188ffb1f33dbd89b2d3af8
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-05 17:42:22 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-6 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} GadgetVerse. Designed & Developed by Abdulqudus.
+      © {new Date().getFullYear()} GadgetVerse. Designed & Developed by <a href="https://aq-portfolio-rose.vercel.app/">Abdulqudus. <a/>
     </footer>
   );
 }
