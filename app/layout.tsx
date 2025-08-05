9f0a691094af7c1865a5d779779a371ec90e72f8
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-05 21:16:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: 'GadgetVerse - Smart Tech, Smarter Design',
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className="pt-20">{children}</main>
+      </body>
     </html>
   );
 }
