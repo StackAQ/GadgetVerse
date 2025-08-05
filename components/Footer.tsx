0013e15601366fde914190699892c78b7e90acee
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-08-05 17:44:39 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
     <footer className="py-6 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} GadgetVerse. Designed & Developed by <a href="https://aq-portfolio-rose.vercel.app/">Abdulqudus. <a/>
+      © {new Date().getFullYear()} GadgetVerse. Designed & Developed by{" "}
+      <a
+        href="https://aq-portfolio-rose.vercel.app/"
+        className="text-cyan-400 hover:underline"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Abdulqudus
+      </a>
+      .
     </footer>
   );
 }
